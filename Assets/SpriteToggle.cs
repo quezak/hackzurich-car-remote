1fae1acff18dc7d718ec82215eead1650133cded
Artur Kozak <artur.kozak2@gmail.com>
2017-09-16 20:19:03 +0200

    plane-agnostic handling

--- Assets/SpriteToggle.cs
+++ Assets/SpriteToggle.cs
@@ -7,7 +7,6 @@ public class SpriteToggle : MonoBehaviour {
 
 	public Sprite sprite1, sprite2;
 	private SpriteRenderer spriteRenderer;
-	private int spriteNo;
 
 	// IT'S A HACKATHON, GO CRAZY
 	public static SpriteToggle INSTANCE;
@@ -20,24 +19,26 @@ public class SpriteToggle : MonoBehaviour {
 
 	public void Appear() {
 		spriteRenderer.enabled = true;
-		spriteNo = 0;
-		spriteRenderer.sprite = sprite1;
+		setForward();
 		Debug.Log("gear selector appears!");
+	}
+
+	private void setBackward() {
+		spriteRenderer.sprite = sprite2;
+		Vehicle.INSTANCE.SetGear(GearDirection.GEAR_DIRECTION_BACKWARD);
+		Debug.Log("gear set to BACKWARD");
+	}
+
+	private void setForward() {
+		spriteRenderer.sprite = sprite1;
+		Vehicle.INSTANCE.SetGear(GearDirection.GEAR_DIRECTION_FORWARD);
+		Debug.Log("gear set to FORWARD");
 	}
 
 	void OnMouseDown() {
 		if (Vehicle.INSTANCE != null) {
-			if (spriteNo == 0) {
-				spriteRenderer.sprite = sprite2;
-				spriteNo = 1;
-				Vehicle.INSTANCE.SetGear(GearDirection.GEAR_DIRECTION_BACKWARD);
-				Debug.Log("gear set to BACKWARD");
-			} else {
-				spriteRenderer.sprite = sprite1;
-				spriteNo = 0;
-				Vehicle.INSTANCE.SetGear(GearDirection.GEAR_DIRECTION_FORWARD);
-				Debug.Log("gear set to FORWARD");
-			}
+			if (spriteRenderer.sprite.Equals(sprite1)) setBackward();
+			else setForward();
 		} else {
 			Debug.Log("no vehicle to shift gears!");
 		}
--- Assets/SteeringInputController.cs
+++ Assets/SteeringInputController.cs
@@ -16,26 +16,38 @@ namespace AssemblyCSharp {
 		float MAX_ANGLE_CHANGE_SEC = 60.0f;
 
 		public void Update() {
-			Quaternion gyroState = Input.gyro.attitude;
-			// Phone horizontal = 90deg reading, right is less
+			Quaternion gyroState = Quaternion(0.5f, 0.5f, -0.5f, 0.5f)
+				* Input.gyro.attitude
+				* new Quaternion(0, 0, 1, 0);
+			Quaternion referenceRotation = Quaternion.identity;
+			Quaternion eliminationOfXY = Quaternion.Inverse(
+				Quaternion.FromToRotation(referenceRotation * Vector3.forward, 
+					gyroState * Vector3.forward)
+			);
+			Quaternion rotationZ = eliminationOfXY * gyroState;
+			float linearAngle = rotationZ.eulerAngles.z;
+
+			//Quaternion gyroState = Input.gyro.attitude;
+			// Phone horizontal = 270deg reading, right is less
 			// Car input with phone horizontal = 0.0, left is less
-			float linearAngle = -(gyroState.eulerAngles.z - 90.0f);
+			//float linearAngle = -(gyroState.eulerAngles.z - 270.0f);
 			if (Math.Abs(linearAngle) < STEERING_DEADZONE_DEG) {
 				linearAngle = 0.0f;
 			} else {
-				linearAngle -= Math.Sign(desiredAngle) * STEERING_DEADZONE_DEG;
+				linearAngle -= Math.Sign(linearAngle) * STEERING_DEADZONE_DEG;
 			}
 			// Apply a non-linear scale, so light turns won't jerk the car
-			desiredAngle = linearAngle * linearAngle / 90.0f * Math.Sign(linearAngle);
+			desiredAngle = linearAngle * linearAngle / 120.0f * Math.Sign(linearAngle);
 			// For big changes, smoothen the steering, don't jump to desired position immediately
 			float maxChange = MAX_ANGLE_CHANGE_SEC * Time.deltaTime;
-			Utils.LogEveryN(1, String.Format("change: {0:F2}, maxChange: {1:F2}", desiredAngle - angle, maxChange));
+			Utils.LogEveryN(10, String.Format("change: {0:F2}, maxChange: {1:F2}", desiredAngle - angle, maxChange));
 			if (Math.Abs(angle - desiredAngle) < maxChange) {
 				angle = desiredAngle;
 			} else {
 				angle += Math.Sign(desiredAngle - angle) * maxChange;
 			}
-			Utils.LogEveryN(1, String.Format("angle: {0:F2} desired: {1:F2}", angle, desiredAngle));
+			Utils.LogEveryN(10, String.Format("angle: {0:F2} desired: {1:F2} raw: {2:F2} linear: {3:F2}",
+				angle, desiredAngle, gyroState.eulerAngles.z, linearAngle));
 
 		}
 
