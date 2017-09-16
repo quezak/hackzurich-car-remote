5f5b863fe4a93a64f208c3443e76bc6be5a300d8
Artur Kozak <artur.kozak2@gmail.com>
2017-09-16 08:31:38 +0200

    add simple steering with 5* deadzone
    works well as long as phone is held vertically in the air

---
+++ Assets/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace AssemblyCSharp {
+	public static class Utils {
+		
+
+		public static void LogEveryN(int frames, String message) {
+			if (Time.frameCount % frames == 0) {
+				Debug.Log(message);
+			}
+		}
+	}
+}
+
